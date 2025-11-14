9b1a48f3d14071f077716d08fade221ac4dcec19
nhilya <nuhailya.dev@gmail.com>
2025-11-14 23:49:15 +0800

    feat: item resource

---
+++ app/Filament/Resources/Items/Schemas/ItemForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Items\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ItemForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Select::make('item_category_id')
+                    ->label('Item Category')
+                    ->relationship('itemCategory', 'display_name')
+                    ->required(),
+                TextInput::make('description')
+                    ->required(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->default(0.00)
+                    ->prefix('RM'),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Items/Schemas/ItemInfolist.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Items\Schemas;
+
+use App\Models\Item;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ItemInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('itemCategory.display_name')
+                    ->label('Item Category'),
+                TextEntry::make('description'),
+                TextEntry::make('price')
+                    ->money(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('deleted_at')
+                    ->dateTime()
+                    ->visible(fn (Item $record): bool => $record->trashed()),
+            ]);
+    }
+}
